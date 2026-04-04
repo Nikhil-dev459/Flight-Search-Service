b50788862cb39933bc1b0745b0827aa450b9749c
Nikhil-dev459 <NKDchamp004@gmail.com>
2026-04-04 15:21:46 +0530

    Added flight-creating service

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const apiRoutes=require('./routes');
 
 const app=express();
 
+app.use(express.json());       //registers a middleware for all upcoming routes, it helps to parse
+                               // the incoming json request body
+app.use(express.urlencoded({extended:true}));   //makes sure to read the url encoded stuff
+
 app.use('/api',apiRoutes);
 
 app.listen(serverConfig.PORT,()=>{
